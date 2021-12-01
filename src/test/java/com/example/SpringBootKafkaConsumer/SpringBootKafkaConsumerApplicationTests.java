80eaf640f5dd66a7f18df549f3bd54f92cc92f38
timour akhounov <timon1983@yandex.ru>
2021-12-01 13:38:13 +0500

    start dev project

---
+++ src/test/java/com/example/SpringBootKafkaConsumer/SpringBootKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootKafkaConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
